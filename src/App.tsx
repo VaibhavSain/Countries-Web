e8afc575cf6a33a22cdfb5c458177b0e9f81f534
Vaibhav <xyzx010101@gmail.com>
2026-07-29 21:54:39 +0530

    updated codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import users from '../src/assets/data.json'
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Filter from "./components/Filter";
+import dashboard from '../src/pages/dashboard.tsx'
+import detail from './pages/detail.tsx'
 
 function App() {
   const [mode, setMode] = useState("bg-[#fafafa]");
   const [search, setSearch] = useState('');
-  const [region, setRegion] = useState('');
-  console.log(region)
+  const [data, setData] = useState(users);
   const changeMode = () => {
     setMode(mode === "bg-[#fafafa]" ? "bg-[#202d36]" : "bg-[#fafafa]");
   };
   return (
     <div className={`h-screen w-screen ${mode}`}>
       <Header changeMode={changeMode} mode={mode} />
-      <Filter setSearch={setSearch} setRegion={setRegion} region={region} />
-      <Card search={search} region={region} />
+      <Filter setSearch={setSearch} />
+      <Card data={data} search={search} />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
-import users from "../assets/data.json";
-function Card({ search }) {
+function Card({ search, data }) {
+  const filterData = data.filter((country) => {
+    return country.name.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <div className="w-full relative top-10 flex pt-10 flex-wrap gap-10 justify-center min-h-screen max-h-max">
-      {users.filter((ind) => {
-        return ind.name.toLowerCase().includes(search.toLowerCase());
-      }).map((ind, key) => (
+      {filterData.map((ind, key) => (
         <div
           key={key}
-          className=" rounded-lg  shadow-gray-300 shadow-lg flex flex-col h-72 w-[18rem]"
+          className="rounded-lg shadow-gray-300 shadow-lg flex flex-col h-72 w-[18rem]"
         >
           <img
             className="w-full bg-cover h-40 rounded-t-lg"
@@ -19,9 +19,9 @@ function Card({ search }) {
             {ind.name}
           </h2>
           <li className="relative list-none top-3 left-4">
-            <p>Population:{ind.population}</p>
-            <p>Region:{ind.region}</p>
-            <p>Capital:{ind.capital}</p>
+            <p>Population: {ind.population}</p>
+            <p>Region: {ind.region}</p>
+            <p>Capital: {ind.capital}</p>
           </li>
         </div>
       ))}
